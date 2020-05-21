e638db8958927ec655c7c90dc26ebf8caa58605d
David Elì <davidelimg1@gmail.com>
2020-05-21 17:57:09 -0500

    Starting working on animation od the menu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import './scss/style.scss';
 import Menu from './menu';
 
 const menu = Menu;
+const btnOne = document.getElementById('icon-1');
+const menuOne =  document.getElementById('menu-1')
+let btnState = false;
 
 const start = (() => {
-  console.log('running...');
+  btnOne.addEventListener('click', (e) => {
+    btnState = !btnState;
+    if(btnState) {
+      menuOne.classList.remove('hide');
+    } else {
+      menuOne.classList.add('hide');
+    }
+  }, false, { once: true });
 })();
