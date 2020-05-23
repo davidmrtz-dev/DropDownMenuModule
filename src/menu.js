20a208ec1a014383a043df063a69c4bac4cd0f48
David Elì <davidelimg1@gmail.com>
2020-05-22 21:33:47 -0500

    Allow multiselect feature added

--- src/menu.js
+++ src/menu.js
@@ -5,23 +5,29 @@ const Menu = (() => {
     const menuElements = document.querySelectorAll('.drop-box-cont');
     let selectedMenus = 0;
     for (const menu in menuElements) {
-      let dropStatus = false;
       if(typeof(menuElements[menu]) === 'number'){ break; }
       menuElements[menu].addEventListener('click', (e) => {
-        dropStatus = !dropStatus;
-        dropStatus ? menuClicked(menuElements[menu]) : menuUnClicked(menuElements[menu]);
-        console.log(dropStatus);
+        if (!multiSelect && !isClicked(menuElements[menu])) {
+          clearSelectedMenus();
+        }
+        isClicked(menuElements[menu]) ?  menuUnClicked(menuElements[menu]) : menuClicked(menuElements[menu]);
       });
     }
   };
 
+  const isClicked = (element) => {
+    let result;
+    element.classList.contains('clicked') ? result = true: result = false;
+    return result;
+  }
+
   const clearSelectedMenus = () => {
     const menuElements = document.querySelectorAll('.drop-box-cont');
     for (const menu in menuElements) {
-      if(typeof(menuElements[menu]) === 'number'){
+      if (typeof(menuElements[menu]) === 'number'){
         break;
       }
-      if(menuElements[menu].classList.contains('clicked')) {
+      if (menuElements[menu].classList.contains('clicked')) {
         menuUnClicked(menuElements[menu]);
       }
     }
