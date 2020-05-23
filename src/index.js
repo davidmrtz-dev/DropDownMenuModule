4e873e2bbd29922d714cdeef22f429a691aeb26e
David Elì <davidelimg1@gmail.com>
2020-05-22 21:18:01 -0500

    Building logic to add menus

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import './scss/style.scss';
 import Menu from './menu';
 
-const menu = Menu;
-const dropCont = document.getElementById('drop-cont');
-let dropStatus = false;
+const menuObj = Menu;
 
 const start = (() => {
-  dropCont.addEventListener('click', (e) => {
-    e.stopPropagation();
-    dropStatus = !dropStatus;
-    if(dropStatus) {
-      dropCont.classList.add('clicked');
-    } else {
-      dropCont.classList.remove('clicked');
-    }
-  });
+  menuObj.scanForMenu();
+  menuObj.allowMultiSelect(false);
 })();
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,49 @@
 const Menu = (() => {
-  
+  let multiSelect = true;
+
+  const scanForMenu = () =>{
+    const menuElements = document.querySelectorAll('.drop-box-cont');
+    let selectedMenus = 0;
+    for (const menu in menuElements) {
+      let dropStatus = false;
+      if(typeof(menuElements[menu]) === 'number'){ break; }
+      menuElements[menu].addEventListener('click', (e) => {
+        dropStatus = !dropStatus;
+        dropStatus ? menuClicked(menuElements[menu]) : menuUnClicked(menuElements[menu]);
+        console.log(dropStatus);
+      });
+    }
+  };
+
+  const clearSelectedMenus = () => {
+    const menuElements = document.querySelectorAll('.drop-box-cont');
+    for (const menu in menuElements) {
+      if(typeof(menuElements[menu]) === 'number'){
+        break;
+      }
+      if(menuElements[menu].classList.contains('clicked')) {
+        menuUnClicked(menuElements[menu]);
+      }
+    }
+  };
+
+  const menuClicked = (element) => {
+    element.classList.add('clicked');
+  };
+
+  const menuUnClicked = (element) => {
+    element.classList.remove('clicked');
+  };
+
+  const allowMultiSelect = (state) => {
+    multiSelect = state;
+  };
+
+
+  return {
+    scanForMenu,
+    allowMultiSelect
+  }
 })();
 
 export default Menu;
